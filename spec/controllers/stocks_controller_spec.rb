d96df497874aea4d6faa0bb7de9a1b1f96b3eda5
Mikey McCormack <mccormack@berkeley.edu>
2016-02-15 12:55:50 -0800

    Switch to single quote strings in specs

--- spec/controllers/stocks_controller_spec.rb
+++ spec/controllers/stocks_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe StocksController, :type => :controller do
-   describe "index" do
+   describe 'index' do
      raw_response = "\"Exxon Mobil Corpo\",\"XOM\",\"NYSE\",7.58,1.11,2.22,3.33,5.432,22\r\n\"Apple Inc.\",\"AAPL\",\"NasdaqNM\",7.65,4.44,5.55,6.66,7.777,55\r\n"
      before(:each) do
        Stock.delete_all
@@ -12,7 +12,7 @@ RSpec.describe StocksController, :type => :controller do
          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
          to_return(:status => 200, :body => raw_response, :headers => {})
      end
-     it "should generate the symbol list, call out to the api, populate the stocks, and return them" do
+     it 'should generate the symbol list, call out to the api, populate the stocks, and return them' do
        expect(Stock.all.size).to eq(0)
        expect_any_instance_of(QuerySetup).to receive(:generate_list).once.and_call_original
        expect_any_instance_of(QuerySetup).to receive(:get_next_200).exactly(75).times.and_call_original
