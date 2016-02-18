715ff2df44530d1d559cfded644849e205491932
Mikey McCormack <mccormack@berkeley.edu>
2016-02-18 00:42:12 -0800

    Add sorting to some columns

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ group :development, :test do
   gem 'byebug'
   gem 'capybara', '>=0'
   gem 'factory_girl'
+  # gem 'factory_girl_rails'
   gem 'poltergeist'
   gem 'rspec', '>=0'
   gem 'rspec-rails'
--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,7 +1,17 @@
-require 'net/http'
-
 class StocksController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
   def index
-    @stocks = Stock.paginate(:page => params[:page], :per_page => 30)
+    @stocks = Stock
+                .order(sort_column + ' ' + sort_direction)
+                .paginate(:page => params[:page], :per_page => 30)
+  end
+
+  def sort_column
+    Stock.column_names.include?(params[:sort]) ? params[:sort] : 'name'
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : 'asc'
   end
 end
