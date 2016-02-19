1f23915ead9f2c1dde4ba2be5c6e49c825819eb2
Mikey McCormack <mccormack@berkeley.edu>
2016-02-19 12:38:59 -0800

    Switch parser from an each to a map

--- app/models/yahoo_raw_response_json_parser.rb
+++ app/models/yahoo_raw_response_json_parser.rb
@@ -1,25 +1,22 @@
 require 'CSV'
 
 class YahooRawResponseJsonParser
-  def self.parse(raw_response)
-    list = []
-    raw_response_array = CSV.parse(raw_response)
-    raw_response_array.each do |stock_obj|
-      list << {
-        name: stock_obj[0],
-        symbol: stock_obj[1],
-        exchange: stock_obj[2],
-        current_eps: stock_obj[3],
-        dividends_per_share: stock_obj[4],
-        day_high_price: stock_obj[5],
-        day_low_price: stock_obj[6],
-        book_value: stock_obj[7],
-        price_to_book_ratio: stock_obj[8],
-        price_to_earnings_ratio: stock_obj[9],
-        year_low_price: stock_obj[10],
-        year_high_price: stock_obj[11]
+  def self.parse(response)
+    CSV.parse(response).map do |stock|
+      {
+        name: stock[0],
+        symbol: stock[1],
+        exchange: stock[2],
+        current_eps: stock[3],
+        dividends_per_share: stock[4],
+        day_high_price: stock[5],
+        day_low_price: stock[6],
+        book_value: stock[7],
+        price_to_book_ratio: stock[8],
+        price_to_earnings_ratio: stock[9],
+        year_low_price: stock[10],
+        year_high_price: stock[11]
       }
     end
-    list
   end
 end
