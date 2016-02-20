7d23b731de1929f46305b5aa2e0693c7aaa61b4f
Mikey McCormack <mccormack@berkeley.edu>
2016-02-19 19:30:47 -0800

    Filter out stocks without data

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,6 +3,7 @@ class StocksController < ApplicationController
 
   def index
     @stocks = Stock
+                .where.not(year_high_price: '')
                 .order(sort_column + ' ' + sort_direction)
                 .paginate(:page => params[:page], :per_page => 30)
   end
