7e6dd0b32be8354f50a13d367d406f1250f7d603
Mikey McCormack <mccormack@berkeley.edu>
2016-03-06 15:39:54 -0800

    Add more to clockwork

--- lib/clock.rb
+++ lib/clock.rb
@@ -4,6 +4,19 @@ require 'clockwork'
 
 include Clockwork
 
-every(10.minutes, 'Running my reoccurring process...') do
+every(24.hours, 'Running my daily process...') do
+  puts 'aggregating stocks...'
   StockDataAggregator.new.aggregate
+end
+
+every(8.hours, 'Running my thrice-daily process...') do
+  puts 'aggregating yahoo key stats...'
+  YahooKeyStatisticsDataAggregator.new.aggregate
+  puts 'aggregating mw data...'
+  MarketWatchDataAggregator.new.aggregate
+  puts 'aggregating bar chart data...'
+  BarChartDataAggregator.new.aggregate
+  puts 'aggregating street insider data...'
+  StreetInsiderDataAggregator.new.aggregate
+  puts 'done!'
 end
